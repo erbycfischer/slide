2df936ef59c36f16d3f3b3e0dcc25c23ce4f7b3b
Erby Fischer <erbycfischer@mac.com>
2019-06-11 16:04:50 -0500

    fixed string url formatting in client

--- client/client.js
+++ client/client.js
@@ -2,6 +2,6 @@ let start = async (ip, outPath) => {
     const request = require('request');
     const fs = require ('fs');
 
-    let response = request("http://ip:8080");
+    let response = request("http://" + ip + ":8080");
     response.pipe(fs.createWriteStream(outPath));
 }
