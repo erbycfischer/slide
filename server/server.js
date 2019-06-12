0b7703ca6f70cb15c0e1fdf65b9914f70ae45a42
Erby Fischer <erbycfischer@mac.com>
2019-06-12 14:36:33 -0500

    Added logging information to the server process

--- server/server.js
+++ server/server.js
@@ -3,9 +3,11 @@ let start = async (port, filePath) => {
     const app = express();
 
     app.get('/', function(req, res) {
+        console.log(req.ip + " is downloading file");
         res.download(filePath);
     })
 
+    console.log("server is serving: " + filePath);
     app.listen(port);
 };
 
